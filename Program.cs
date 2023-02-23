5bca008b6f887d94df9fd7457528f04c8386d636
shtv_oleg <ov09122014@mail.ru>
2023-02-23 19:34:55 +0400

    Lesson_9_Task_68

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ int Ackerman(int m, int n)                                      // метод н
     if (m == 0)                                                 //  условие выхода из рекурсии
         return n + 1;
     else
-      if (m != 0 && n == 0)
+        if (m != 0 && n == 0)
         return Ackerman(m - 1, 1);
     else
         return Ackerman(m - 1, Ackerman(m, n - 1));
@@ -18,4 +18,7 @@ Console.WriteLine("Введите число m: ");	                        //  
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число n: ");	                        //  запрос второго числа
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Функция Аккермана для аргументов {m} и {n} равна: {Ackerman(m, n)}.");
+if (m > 0 && n > 0)
+    Console.WriteLine($"Функция Аккермана для аргументов {m} и {n} равна: {Ackerman(m, n)}.");
+else
+    Console.WriteLine("Числа m и n должны быть больше 0.");
